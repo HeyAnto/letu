8749311603e106ba35a1d7548bd2bad7fe05f1fd
HeyAnto <antonin63170@gmail.com>
2025-04-09 15:31:17 +0200

    Add Ingredient Delete

--- src/Controller/AdminIngredientController.php
+++ src/Controller/AdminIngredientController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\IngredientRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,9 +12,19 @@ use Symfony\Component\Routing\Attribute\Route;
 final class AdminIngredientController extends AbstractController
 {
     #[Route('', name: 'admin_ingredient_index')]
-    public function index(): Response
+    public function index(IngredientRepository $ingredientRepository): Response
     {
-        return $this->render('admin/admin_ingredient/index.html.twig');
+        $ingredients = $ingredientRepository->findAll();
+
+        return $this->render('admin/admin_ingredient/index.html.twig', [
+            'ingredients' => $ingredients,
+        ]);
+    }
+
+    #[Route('/add/ingredient', name: 'admin_ingredient_add')]
+    public function addRecipe(): Response
+    {
+        return $this->render('admin/admin_ingredient/add.html.twig');
     }
 
     #[Route('/{id}', name: 'admin_ingredient_show')]
@@ -21,9 +33,13 @@ final class AdminIngredientController extends AbstractController
         return $this->render('admin/admin_ingredient/show.html.twig');
     }
 
-    #[Route('/add/recipe', name: 'admin_ingredient_add')]
-    public function addRecipe(): Response
+    #[Route('/delete/ingredient/{id}', name: 'admin_ingredient_delete')]
+    public function delete(IngredientRepository $ingredientRepository, EntityManagerInterface $entityManager, int $id): Response
     {
-        return $this->render('admin/admin_ingredient/add.html.twig');
+        $ingredient = $ingredientRepository->find($id);
+        $entityManager->remove($ingredient);
+        $entityManager->flush();
+
+        return $this->render('admin/admin_ingredient/index.html.twig');
     }
 }
