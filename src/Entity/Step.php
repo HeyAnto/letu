596ac92d760e46c9217b7d626cbf278b5245f284
HeyAnto <antonin63170@gmail.com>
2025-04-09 11:04:41 +0200

    Save

--- src/Entity/Step.php
+++ src/Entity/Step.php
@@ -2,10 +2,11 @@
 
 namespace App\Entity;
 
-use App\Repository\StepRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
+use App\Entity\Ingredient;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\StepRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 
 #[ORM\Entity(repositoryClass: StepRepository::class)]
 class Step
