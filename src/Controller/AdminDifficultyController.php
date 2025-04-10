35e43c7db0d5a52471a843260c10a18386f5eb7e
HeyAnto <antonin63170@gmail.com>
2025-04-10 09:30:16 +0200

    BO Index & Delete Difficulty

--- src/Controller/AdminDifficultyController.php
+++ src/Controller/AdminDifficultyController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\DifficultyRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/admin/difficulty')]
 final class AdminDifficultyController extends AbstractController
 {
     #[Route('', name: 'admin_difficulty_index')]
-    public function index(): Response
+    public function index(DifficultyRepository $difficultyRepository): Response
     {
-        return $this->render('admin/admin_difficulty/index.html.twig');
+        $difficulties = $difficultyRepository->findAll();
+
+        return $this->render('admin/admin_difficulty/index.html.twig', [
+            'difficulties' => $difficulties,
+        ]);
+    }
+
+    #[Route('/add/difficulty', name: 'admin_difficulty_add')]
+    public function addRecipe(): Response
+    {
+        return $this->render('admin/admin_difficulty/add.html.twig');
     }
 
     #[Route('/{id}', name: 'admin_difficulty_show')]
@@ -21,9 +33,13 @@ final class AdminDifficultyController extends AbstractController
         return $this->render('admin/admin_difficulty/show.html.twig');
     }
 
-    #[Route('/add/recipe', name: 'admin_difficulty_add')]
-    public function addRecipe(): Response
+    #[Route('/delete/difficulty/{id}', name: 'admin_difficulty_delete')]
+    public function delete(DifficultyRepository $difficultyRepository, EntityManagerInterface $entityManager, int $id): Response
     {
-        return $this->render('admin/admin_difficulty/add.html.twig');
+        $difficulty = $difficultyRepository->find($id);
+        $entityManager->remove($difficulty);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('admin_difficulty_index');
     }
 }
