e9dba5a2b071971643aa9ea728506bb448caceb8
HeyAnto <antonin63170@gmail.com>
2025-04-10 23:43:38 +0200

    Add Recipe Card

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,17 +2,38 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Repository\RecipeRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class IndexController extends AbstractController
 {
     #[Route('', name: 'index')]
-    public function index(): Response
+    public function index(RecipeRepository $recipeRepository): Response
     {
+        $user = $this->getUser();
+        $recipes = $recipeRepository->findBy([], ['createdAt' => 'DESC'], 9);
+
         return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
+            'recipes' => $recipes,
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/recipe/{id}', name: 'recipe_show')]
+    public function showRecipe(int $id, RecipeRepository $recipeRepository): Response
+    {
+        $recipe = $recipeRepository->find($id);
+
+        if (!$recipe) {
+            return $this->redirectToRoute('index');
+        }
+
+        return $this->render('index/show.html.twig', [
+            'recipe' => $recipe,
+            'user' => $this->getUser(),
         ]);
     }
 }
